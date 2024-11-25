1a3f9a19ff34f0d4cbc9be230e3c2ced23e8cbb7
sungmi0131 <sungmi013100@gmail.com>
2024-11-25 14:56:09 +0900

    background 추가

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { menu } from "./src/js/menu.js";
 import { link } from "./src/js/link.js";
+import { bgColor } from "./src/js/bg.color.js";
 
 window.addEventListener('load',function(){
     menu()
     link()
+    bgColor()
 })
